8ac425739943aa6785674df3ada8019089e852d7
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-10-29 19:34:53 -0500

    remove front end folder.

---
+++ src/Components/FlashcardContainer/FlashcardContainer.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Flashcard from "../Flashcard/Flashcard";
+
+export default class FlashcardContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      flashcardList: [],
+    };
+  }
+
+  async componentDidMount() {
+    await this.getFlashcardList();
+  }
+
+  async getFlashcardList() {
+    if (!this.props.selection.length) return;
+    const flashcards = await this.props.getFlashcards(this.props.selection);
+
+    this.setState({
+      flashcardList: flashcards,
+    });
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        {this.props.selection.length > 0 &&
+          this.state.flashcardList.map((flashcard, index) => (
+            <Flashcard
+              key={flashcard._id}
+              word={flashcard.word}
+              definition={flashcard.definition}
+              length={this.state.flashcardList.length}
+              index={index}
+            />
+          ))}
+      </div>
+    );
+  }
+}
